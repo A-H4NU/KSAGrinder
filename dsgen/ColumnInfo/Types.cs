f13acfadce61a3883f3393184cecc71e142bf9af
Seungwoo Han <27879629+A-H4NU@users.noreply.github.com>
2023-07-22 01:40:24 +0900

    (no syntax change)

--- dsgen/ColumnInfo/Types.cs
+++ dsgen/ColumnInfo/Types.cs
@@ -8,13 +8,13 @@ using T = System.Collections.ObjectModel.ReadOnlyCollection<Type?>;
 public static class Types
 {
     public static readonly T Code = new Type?[] { typeof(String) }.AsReadOnly();
-    public static readonly T Department = (new Type?[] { typeof(String) }).AsReadOnly();
-    public static readonly T Name = (new Type?[] { typeof(String) }).AsReadOnly();
-    public static readonly T Grade = (new Type?[] { typeof(Double) }).AsReadOnly();
-    public static readonly T Class = (new Type?[] { typeof(Double) }).AsReadOnly();
-    public static readonly T Teacher = (new Type?[] { typeof(String) }).AsReadOnly();
-    public static readonly T Time = (new Type?[] { typeof(String) }).AsReadOnly();
-    public static readonly T Enrollment = (new Type?[] { typeof(Double) }).AsReadOnly();
-    public static readonly T Credit = (new Type?[] { typeof(Double) }).AsReadOnly();
-    public static readonly T Note = (new Type?[] { typeof(String), null }).AsReadOnly();
+    public static readonly T Department = new Type?[] { typeof(String) }.AsReadOnly();
+    public static readonly T Name = new Type?[] { typeof(String) }.AsReadOnly();
+    public static readonly T Grade = new Type?[] { typeof(Double) }.AsReadOnly();
+    public static readonly T Class = new Type?[] { typeof(Double) }.AsReadOnly();
+    public static readonly T Teacher = new Type?[] { typeof(String) }.AsReadOnly();
+    public static readonly T Time = new Type?[] { typeof(String) }.AsReadOnly();
+    public static readonly T Enrollment = new Type?[] { typeof(Double) }.AsReadOnly();
+    public static readonly T Credit = new Type?[] { typeof(Double) }.AsReadOnly();
+    public static readonly T Note = new Type?[] { typeof(String), null }.AsReadOnly();
 }
